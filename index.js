d17f2e91c1d5f0b3091e210cd2e11f06b91cc77d
Juan Augusto <83130876+Juan-Augusto@users.noreply.github.com>
2022-05-04 11:25:42 -0300

    Final

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+let results = []
+let victories = []
 function game() {
   let userChoice = document.getElementById("user-choice").value;
+  let gamesPlayed = document.getElementById("games-played");
   let computerResult = document.getElementById("computer-choice");
+  let victoryPercentage =   document.getElementById("victory-percentage");
+
   let result = document.getElementById("result");
-  result.classList.remove('draw', 'win', 'lose');
-  let results = []
   let choose = ["Pedra", "Papel", "Tesoura"];
   let computerChoice = choose[Math.floor(Math.random() * choose.length)];
+  result.classList.remove('draw', 'win', 'lose');
 
-  console.log(userChoice, computerChoice);
   if (userChoice === computerChoice) {
     results.push("Empate");
     result.classList.toggle("draw");
@@ -15,23 +18,33 @@ function game() {
     result.innerHTML = `<h2>Foi um empate!</h2>`;
 
   } else if (userChoice === "Pedra" && computerChoice === "Tesoura") {
+    results.push("Vitória");
+    victories.push("Vitória");
     result.classList.toggle("win");
     computerResult.innerHTML = `<h2>Escolha da máquina: ${computerChoice}<h2>`;
     result.innerHTML = `<h2>Você Ganhou!<h2>`;
 
   } else if (userChoice === "Papel" && computerChoice === "Pedra") {
+    results.push("Vitória");
+    victories.push("Vitória");
     result.classList.toggle("win");
     computerResult.innerHTML = `<h2>Escolha da máquina: ${computerChoice}<h2>`;
     result.innerHTML = `<h2>Você Ganhou!<h2>`;
 
   } else if (userChoice === "Tesoura" && computerChoice === "Papel") {
+    results.push("Vitória");
+    victories.push("Vitória");
     result.classList.toggle("win");
     computerResult.innerHTML = `<h2>Escolha da máquina: ${computerChoice}<h2>`;
     result.innerHTML = `<h2>Você Ganhou!<h2>`;
 
   } else {
+    results.push("Derrota");
     result.classList.toggle("lose");
     computerResult.innerHTML = `<h2>Escolha da máquina: ${computerChoice}<h2>`;
     result.innerHTML = `<h2>Você Perdeu!<h2>`;
   }
+  
+  results.length === 1 ? gamesPlayed.innerHTML = `<h2>Você jogou ${results.length} vez</h2>` : gamesPlayed.innerHTML = `<h2>Você jogou ${results.length} vezes</h2>`;
+  victoryPercentage.innerHTML = `<h2>Percentual de vitória: ${(victories.length / results.length) * 100}</h2>`;
 }
